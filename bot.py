d9b419492b8cdeabbd39cc907a938a791a2b5d37
Nugtrix-DT <nugtrixdt@gmail.com>
2021-06-29 15:41:48 -0500

    add: Added bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,25 @@
 from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def start(update, context):
 
-    update.message.reply_text('Hola, Esclavo')
+    button1 = InlineKeyboardButton(
+        text='Nugtrix-DT',
+        url='https://api.whatsapp.com/send/?phone=593978963202&text&app_absent=0'
+    )
+
+    update.message.reply_text(
+         text='Hola, Esclavo',
+         reply_markup=InlineKeyboardMarkup([
+             [button1]
+         ])
+    )
 
 
 if __name__ == '__main__':
 
-    updater = Updater(token='1823188219:AAGrfAdDAFvr38COWGThFFnx2CJv2nrYZPo', use_context=True)
+    updater = Updater(token='YOUR TOKEN', use_context=True)
 
     dp = updater.dispatcher
 
